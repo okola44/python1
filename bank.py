9f7a2e8235edae4a6a5cd3e709a1fc6618458bf5
Okola44 <mercyimaliokola@gmail.com>
2021-05-31 19:30:02 +0300

    commit

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class BankAccount:
     fixedAccount="fixed"
     savingsAccount="save"
@@ -6,6 +7,7 @@ class BankAccount:
         self.phonenumber=phonenumber
         self.balance=0
         self.loan=0
+        self.statement=[]#instasiating statement as an empty list so we can add values
 
     def withdraw(self):
         return f"please confirm your withdrawal from account {self.name}"
@@ -16,12 +18,27 @@ class BankAccount:
             return f"you cannot deposit less than 0"
         else:
              self.balance+=amount
+             now=datetime.now()
+             transaction={"amount":{amount},"time":now,"narration":"you have deposited"}#a dictionary with transaction details
+             self.statement.append(transaction)#appending transactions in the empty list statements
         return self.show_balance()
+
+    def show_statement(self):
+        for transaction in self.statement:
+            amount=transaction["amount"]
+            narration=transaction["narration"]
+            time=transaction["time"]
+            date=time.strftime("%d/%m/%y")
+            print(f"{date}:{narration} {amount}")
+        return  
     def withdraw(self,amount):
         if amount>self.balance:
             return f"your balance is {self.balance} you cannot withdraw{amount}"
         else:
             self.balance-=amount
+            now=datetime.now()
+            withdrawals={"amount":{amount},"time":now,"narration":"you have withdrawn"}#a dictionary with transaction details
+            self.statement.append(withdrawals)
         return self.show_balance()
 
     def borrow(self,amount):
@@ -29,6 +46,8 @@ class BankAccount:
 
     def repayloan(self,amount):
             return f"you have repaid your loan of ksh{amount}"
+
+
     
 
 
--- dog.py
+++ dog.py
@@ -8,6 +8,15 @@ class Dog:
         return f"My {self.breed} goes like bark!bark!bark! "
     def sniff(self):
         return f"My {self.name}   can be trained to sniff"
+        #from modle import class
+        #from module iimport func name
+        #from module.module import class
+        #you can import a whole module eg import student
+        #use dir ()to find out what the module does(bilt in modules)
+        #datetime.now() gives us the real time according to your computers date
+        
+
+
 
 
     
